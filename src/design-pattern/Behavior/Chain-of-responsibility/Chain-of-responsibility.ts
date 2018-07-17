83be30619b7eafeecedb45bad97eb4784fbbe4d7
jsm1003 <838432498@qq.com>
2018-07-17 15:37:16 +0800

    Add chain of responsibility

---
+++ src/design-pattern/Behavior/Chain-of-responsibility/Chain-of-responsibility.ts
@@ -0,0 +1,32 @@
+/**
+ * Chain of responsibility 职责链模式
+ * Whether it's scope chain, the prototype chain, and event bubbling is in the DOM node,
+ * 无论是作用域链，原型链，还是 DOM 节点中的事件冒泡
+ * we can find the shadow of the Chain of Responsibility pattern
+ * 我们都能从中找到职责链模式的影子
+ */
+// tslint:disable:no-any
+
+export class Chain {
+  private readonly mark: string = 'nextProcess';
+  // next node in the chain of responsibility
+  private next: Chain;
+  // current function in the chain
+  private handler: (...rest: any[]) => any;
+  public constructor(func: (...rest: any[]) => any) {
+    this.handler = func;
+  }
+
+  public setNextProcess(chain: Chain): void {
+    this.next = chain;
+  }
+
+  public processRequest(...rest: any[]): void | string {
+    const result: string = this.handler.apply(this, arguments);
+    if (result === this.mark) {
+      return this.next && this.next.processRequest.apply(this.next, arguments);
+    }
+
+    return result;
+  }
+}
---
+++ src/design-pattern/Behavior/Chain-of-responsibility/Demo.ts
@@ -0,0 +1,32 @@
+import { Chain } from './Chain-of-responsibility';
+
+/**
+ * Demo
+ */
+
+function validator(num: number): string | void {
+  if (num <= 10) {
+    console.log('检查通过');
+
+    return 'nextProcess';
+  } else {
+    console.log('检查不通过');
+  }
+}
+
+function checkPermission(orderState: number): string | void {
+  if (orderState === 6) {
+    console.log('订单状态正常，可以下单');
+
+    return 'nextProcess';
+  } else {
+    console.log('订单状态异常');
+  }
+}
+
+const validatorChain: Chain = new Chain(validator);
+const checkPermissionChain: Chain = new Chain(checkPermission);
+
+validatorChain.setNextProcess(checkPermissionChain);
+
+validatorChain.processRequest(11);
